98fb4afc2013d278736623917dc56fd803e494a0
JIH2008 <jihadmunjed2008@gmail.com>
2022-06-19 13:41:45 +0300

    jihad first program

--- index.ts
+++ index.ts
@@ -6,4 +6,4 @@ let num1, num2, num3: number;
 num1 = parseInt(prompt('please add number 1'));
 num2 = parseInt(prompt('please add number 2'));
 num3 = num1 + num2;
-console.log(mun3);
+console.log(num3);
